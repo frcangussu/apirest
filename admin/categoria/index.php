a5baaf13633bedae858db6f74eba8ef8540e810f
Fernando R. Cangussu <frcangussu@gmail.com>
2016-08-31 21:54:00 -0300

    alterando conexão com o banco de dados

--- admin/categoria/index.php
+++ admin/categoria/index.php
@@ -20,7 +20,7 @@ function validaExecucao($rslt,$operacao){
 
 function getConn(){
 	try {
-		$connection = new PDO('mysql:dbname=CadastroLogin;host=127.0.0.1', 'root', 'root', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+		$connection = new PDO('mysql:dbname=portalmangueiral;host=127.0.0.1', 'root', '123456', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		return $connection;
 	} catch (PDOException $e) {
